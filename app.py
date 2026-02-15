dd075f69f5518ce57f701c69e08f355f6c6d736c
greenscrocodile <greenscrocodile@gmail.com>
2026-02-15 19:52:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -251,11 +251,10 @@ if st.session_state.locked:
 
                     if st.form_submit_button("Add to Batch"):
                         if bank_name and re.match(r"^\d{6}$", inst_no):
-                            cleaned_month = display_month_text[:-3]
                             # Append the final record to the batch list
                             st.session_state.all_receipts.append({
                                 'id': str(uuid.uuid4()), 'challan': next_no, 'pdate': st.session_state.formatted_pdate,
-                                'name': row['Name'], 'num': row['Consumer Number'], 'month': cleaned_month, 
+                                'name': row['Name'], 'num': row['Consumer Number'], 'month': display_month_text, 
                                 'amount': format_indian_currency(total_amt), 
                                 'words': num2words(total_amt, lang='en_IN').replace(",", "").replace(" And ", " and ").title().replace(" And ", " and "),
                                 'pay_type': mode, 'pay_no': inst_no, 'bank': bank_name, 'date': inst_date.strftime("%d.%m.%Y")
@@ -298,6 +297,7 @@ if st.session_state.locked:
             doc.render({'receipts': st.session_state.all_receipts})
             output = io.BytesIO(); doc.save(output)
             st.download_button("📥 Download", output.getvalue(), file_name=f"Challans_{date.today()}.docx")
+
 
 
 
